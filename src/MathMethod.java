d47367d19f672b2cf7f5f31944fa1ddb1c04c635
M A Mannan <mdmannan580@gmail.com>
2023-11-17 10:01:50 +0300

    Math Method

---
+++ src/MathMethod.java
@@ -0,0 +1,16 @@
+public class MathMethod {
+    public static void main(String[] args) {
+        int x=2;
+        int y=2;
+
+      int max= Math.max(x,y);
+        System.out.println("Maximum = "+max);
+        int min=Math.min(x,y);
+        System.out.println("Minimum = "+min);
+        int abs=Math.abs(y);
+        System.out.println("Absolute of Y = "+abs);
+        double pow=Math.pow(x,y);
+        System.out.println("Power x of Y = "+pow);
+        double PI=Math.E;
+    }
+}
