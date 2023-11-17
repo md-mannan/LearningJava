c4fd49eb7b8d6494ac06195eca7ad4f96525c246
M A Mannan <mdmannan580@gmail.com>
2023-11-17 09:40:41 +0300

    Bitwise operator

--- src/BitwiseOparator.java
+++ src/BitwiseOparator.java
@@ -1,2 +1,24 @@
 public class BitwiseOparator {
+    public static void main(String[] args) {
+        int num1=32;
+        int num2 =12;
+        int c;
+        c=num1&num2;
+        System.out.println("num1 & num2= "+c);
+        c=num1|num2;
+        System.out.println("num1 | num2= "+c);
+        c=num1^num2;
+        System.out.println("num1 ^ num2= "+c);
+        c=num1>>2;
+        System.out.println("num1 >> 2= "+c);
+        c=num1<<2;
+        System.out.println("num1 >> 2= "+c);
+        c=num1>>2;
+        System.out.println("num1 >> 2= "+c);
+        c=~num1;
+        System.out.println("~num1= "+c);
+
+
+
+    }
 }
